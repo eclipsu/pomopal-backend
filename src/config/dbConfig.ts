3d6f04f1d835480427ef2e588afc8eef643a8fe4
eclipsu <stharj219@gmail.com>
2026-01-02 09:41:03 -0600

    Fix(deployment) - Added connection via database url instead

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -2,16 +2,15 @@ import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConne
 
 export default (): PostgresConnectionOptions => ({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  // host: process.env.DB_HOST,
+  // port: Number(process.env.DB_PORT) || 5432,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   synchronize: true,
   migrationsRun: true,
-  ssl: {
-    rejectUnauthorized: false, // Required for Koyeb deployment
-  },
+  ssl: true,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
 });
