050bfa880529052f05efe20cc88fb4f8af1a17ad
eclipsu <stharj219@gmail.com>
2026-01-02 23:46:50 -0600

    attempt to fix ssl error

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  (app.getHttpAdapter().getInstance() as any).set('trust proxy', 1);
 
   const port = Number(process.env.PORT) || 8000;
   await app.listen(port, '0.0.0.0');
